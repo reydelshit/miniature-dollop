ff5196a6edd4a429a74c1fa648f899748d5fa7d4
Reydel Ocon <reydel321@gmail.com>
2024-09-18 01:49:58 +0800

    omg

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -27,7 +27,7 @@ const app = (0, express_1.default)();
 const PORT = process.env.PORT || 8800;
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
     credentials: true,
 }));
 app.use((0, cookie_parser_1.default)());
