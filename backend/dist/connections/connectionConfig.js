49aadedea8d1a70c463e3d48e7a6800ec7b78c4e
Reydel Ocon <reydel321@gmail.com>
2024-09-18 00:38:02 +0800

    uncomment dist

---
+++ backend/dist/connections/connectionConfig.js
@@ -0,0 +1,23 @@
+"use strict";
+// export const connectionConfig = {
+//   server: 'REYDELSHIT',
+//   database: 'LIVEWELL',
+//   options: {
+//     trustedConnection: true,
+//     trustServerCertificate: true,
+//   },
+//   driver: 'msnodesqlv8',
+// };
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.connectionConfig = void 0;
+exports.connectionConfig = {
+    server: 'mssql-183603-0.cloudclusters.net',
+    port: 10077,
+    database: 'LIVEWELLCLOUD',
+    user: 'admin',
+    password: 'Admin123',
+    options: {
+        // trustedConnection: true,
+        trustServerCertificate: true,
+    },
+};
