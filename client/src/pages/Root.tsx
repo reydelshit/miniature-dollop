90a35f0d15e9a41c26e169aecefdc69a1aea5559
Reydel Ocon <reydel321@gmail.com>
2024-09-17 21:55:27 +0800

    remove useffect in root

--- client/src/pages/Root.tsx
+++ client/src/pages/Root.tsx
@@ -1,16 +1,9 @@
 import { Toaster } from '@/components/ui/toaster';
-import { useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import HeroPage from './HeroPage';
 
 const Root = () => {
   const params = useLocation();
-
-  useEffect(() => {
-    if (!localStorage.getItem('isLoggedIn_QR')) {
-      window.location.href = '/login';
-    }
-  }, []);
 
   return (
     <div className="flex h-full w-full items-center justify-center">
