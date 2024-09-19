e3625d7284b7b282d62f41b18d783c9481a57450
Reydel Ocon <reydel321@gmail.com>
2024-09-19 21:20:24 +0800

    changes in env

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -35,7 +35,7 @@ app.use((0, cors_1.default)({
 }));
 app.use((0, cookie_parser_1.default)());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.NODE_ENV === 'PROD' ? process.env.PROD_CLIENT_URL : '*');
+    res.header('Access-Control-Allow-Origin', process.env.PROD_CLIENT_URL);
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
--- backend/index.ts
+++ backend/index.ts
@@ -27,10 +27,7 @@ app.use(
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Origin',
-    process.env.NODE_ENV === 'PROD' ? process.env.PROD_CLIENT_URL : '*',
-  );
+  res.header('Access-Control-Allow-Origin', process.env.PROD_CLIENT_URL);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header(
